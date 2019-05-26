22584e7e5a1c8c223c348aa633dd0f0695e76e3a
val17102 <val17102@uvg.edu.gt>
2019-05-26 17:56:11 -0600

    Video

--- app/src/main/java/com/example/antonio/hciluces/Video.java
+++ app/src/main/java/com/example/antonio/hciluces/Video.java
@@ -12,12 +12,22 @@ public class Video extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-        VideoView videoView= findViewById(R.id.video_view);
+        final Boolean bool;
+        final VideoView videoView= findViewById(R.id.video_view);
         String videoPath = "android.resource://" + getPackageName() + "/"+ R.raw.video;
         Uri uri = Uri.parse(videoPath);
         videoView.setVideoURI(uri);
         MediaController mediaController = new MediaController(this);
         videoView.setMediaController(mediaController);
         mediaController.setAnchorView(videoView);
+
+        android.support.v7.widget.CardView controlButton = findViewById(R.id.play);
+        controlButton.setOnClickListener(new android.view.View.OnClickListener(){
+            public void onClick(android.view.View v){
+                videoView.start();
+
+            }
+        });
+
     }
 }
